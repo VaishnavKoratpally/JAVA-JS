01b64749900e922a474515fd64c2680d13596185
Vaishnav Koratpally <vaishnav.koratpally@gmail.com>
2023-06-02 11:18:13 -0500

    Commit from intellij

--- JAVA/Assignment2/src/Main.java
+++ JAVA/Assignment2/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("Original price is "+product.getPrice());
         System.out.println("price with quantity is "+product.getPrice(4));
         Runtime runtime=new Runtime();
-        System.out.println("Overridden method " +runtime.Demo());
+        System.out.println("Overridden method is " +runtime.Demo());
 
 
     }
